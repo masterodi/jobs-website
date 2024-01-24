1dff4254466482d033e8a4e9333b3a577a48833f
mrheio <drg.ov00@gmail.com>
2024-01-24 22:58:05 +0200

    feat: Small css update

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer class="grid min-h-[40vh] place-items-center bg-indigo-900">
       <div class="container mx-auto">
-        <ul class="flex justify-center gap-8 text-xl text-white">
+        <ul class="flex justify-center gap-16 text-xl text-white">
           <li>
             <a href="#" class="transition-all duration-300 ease-in-out hover:text-indigo-100">
               About
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 const Card = (props: any) => {
-  return <div class="flex flex-col rounded-lg bg-gray-100 px-4 py-16">{props.children}</div>;
+  return <div class="flex flex-col rounded-lg bg-indigo-900 fill-white px-4 py-16 text-white">{props.children}</div>;
 };
 
 export default Card;
