7d3acae52c80daa888842379aadb464e91c8dee1
mrheio <drg.ov00@gmail.com>
2024-03-03 00:09:46 +0200

    fix: Update focus highlight on Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ const Input: Component<InputProps> = (props) => {
   const [local, others] = splitProps(props, ['class', 'classList', 'invalid', 'fluid']);
   return (
     <input
-      class="rounded-md bg-primary-500/25 p-2 focus:bg-primary-500/35"
+      class="rounded-md bg-primary-500/25 p-2 focus:bg-primary-500/35 focus:outline-primary-400"
       classList={{ 'border-2 border-red-500': local.invalid, 'w-full': local.fluid }}
       aria-invalid={local.invalid}
       {...others}
