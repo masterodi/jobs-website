4b557dc24eca48a62cf72361115235946c44a07c
mrheio <drg.ov00@gmail.com>
2024-03-02 21:11:49 +0200

    feat: Add filtering options to jobs fetch

--- src/api/jobs.ts
+++ src/api/jobs.ts
@@ -1,9 +1,49 @@
-import { collection, getDocs, query } from 'firebase/firestore';
+import { OrderByDirection, collection, getDocs, orderBy, query, where } from 'firebase/firestore';
 import { firestore } from '../firebase';
-import { Job } from '../types';
+import { Job, JobsOrder } from '../types';
 
-export const getJobs = async () => {
-  const q = query(collection(firestore, 'jobs'));
+export const JOBS_ORDER = {
+  JOB_TITLE: { ASC: 'JOBS_ORDER:JOB_TITLE:ASC', DESC: 'JOBS_ORDER:JOB_TITLE:DESC' },
+};
+
+export const getJobs = async (filters?: {
+  order?: JobsOrder;
+  skills?: string[];
+  industries?: string[];
+  employmentTypes?: string[];
+}) => {
+  let q = query(collection(firestore, 'jobs'));
+
+  let o = { by: 'job_title', dir: 'asc' };
+
+  if (filters) {
+    const { order, skills, industries, employmentTypes } = filters;
+
+    if (order === 'JOBS_ORDER:JOB_TITLE:ASC') {
+      o = { by: 'job_title', dir: 'asc' };
+    }
+
+    if (order === 'JOBS_ORDER:JOB_TITLE:DESC') {
+      o = { by: 'job_title', dir: 'desc' };
+    }
+
+    if (o) {
+      q = query(q, orderBy(o.by, o.dir as OrderByDirection));
+    }
+
+    if (skills && skills.length) {
+      q = query(q, where('required_skills', 'array-contains-any', skills));
+    }
+
+    if (industries && industries.length) {
+      q = query(q, where('industry', 'in', industries));
+    }
+
+    if (employmentTypes && employmentTypes.length) {
+      q = query(q, where('employment_type', 'in', employmentTypes));
+    }
+  }
+
   const qs = await getDocs(q);
   const jobs = qs.docs.map(
     (doc) =>
@@ -13,4 +53,51 @@ export const getJobs = async () => {
       }) as Job,
   );
   return jobs;
+};
+
+export const getJobsSkills = async () => {
+  const jobs = await getJobs();
+  const skills = [] as string[];
+
+  for (const job of jobs) {
+    const { required_skills } = job;
+
+    for (const skill of required_skills) {
+      if (!skills.includes(skill)) {
+        skills.push(skill);
+      }
+    }
+  }
+
+  return skills.sort();
+};
+
+export const getJobsIndustries = async () => {
+  const jobs = await getJobs();
+  const industries = [] as string[];
+
+  for (const job of jobs) {
+    const { industry } = job;
+
+    if (!industries.includes(industry)) {
+      industries.push(industry);
+    }
+  }
+
+  return industries.sort();
+};
+
+export const getJobsEmploymentTypes = async () => {
+  const jobs = await getJobs();
+  const types = [] as string[];
+
+  for (const job of jobs) {
+    const { employment_type } = job;
+
+    if (!types.includes(employment_type)) {
+      types.push(employment_type);
+    }
+  }
+
+  return types.sort();
 };
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,4 @@ export type User = InferType<typeof userSchema>;
 export type Job = InferType<typeof jobSchema>;
 export type SigninData = InferType<typeof signinSchema>;
 export type RegisterData = InferType<typeof registerSchema>;
+export type JobsOrder = 'JOBS_ORDER:JOB_TITLE:ASC' | 'JOBS_ORDER:JOB_TITLE:DESC';
