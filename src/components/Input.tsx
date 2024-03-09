b3e5349c3a90de3c8efbc1a386b648c9ff2eccfd
mrheio <drg.ov00@gmail.com>
2024-03-09 08:54:11 +0200

    refactor: Update Input to accept complex scenarios

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,44 @@
-import { Component, JSX, splitProps } from 'solid-js';
+import { Component, JSX, Show, splitProps } from 'solid-js';
 
-export type InputProps = JSX.InputHTMLAttributes<HTMLInputElement> & {
+export type SimpleInputProps = JSX.InputHTMLAttributes<HTMLInputElement> & {
   invalid?: boolean;
   fluid?: boolean;
 };
 
-const Input: Component<InputProps> = (props) => {
+const SimpleInput: Component<SimpleInputProps> = (props) => {
   const [local, others] = splitProps(props, ['class', 'classList', 'invalid', 'fluid']);
   return (
     <input
-      class="rounded-md bg-primary-500/25 p-2 focus:bg-primary-500/35 focus:outline-primary-400"
+      class="rounded-md border-2 border-transparent bg-primary-500/25 px-4 py-3 outline-none focus:border-primary-500 focus:bg-primary-500/35"
       classList={{ 'border-2 border-red-500': local.invalid, 'w-full': local.fluid }}
       aria-invalid={local.invalid}
       {...others}
     />
   );
+};
+
+const ComposedInput = (props: any) => {
+  const [local, others] = splitProps(props, ['content', 'invalid', 'fluid']);
+
+  return (
+    <div
+      class="flex items-center gap-2 rounded-md border-2 border-transparent bg-primary-500/25 px-4 py-2 focus-within:border-primary-500 focus-within:bg-primary-500/35 focus-within:outline-primary-500"
+      classList={{ 'border-2 border-red-500': local.invalid, 'w-full': local.fluid }}
+      aria-invalid={local.invalid}
+    >
+      <Show when={local.content?.left}>{local.content.left}</Show>
+      <input class="w-full bg-transparent p-1 outline-none" {...others} />
+      <Show when={local.content?.right}>{local.content.right}</Show>
+    </div>
+  );
+};
+
+const Input = (props: any) => {
+  if (props.content) {
+    return <ComposedInput {...props} />;
+  }
+
+  return <SimpleInput {...props} />;
 };
 
 export default Input;
