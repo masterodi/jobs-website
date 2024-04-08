ab54fc3a81d7bfde5b5552497b601ab85af48882
mrheio <drg.ov00@gmail.com>
2024-04-08 10:00:25 +0300

    refactor: Update App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import { Route, Router } from '@solidjs/router';
 import Layout from './Layout';
+import Dashboard from './pages/dashboard';
 import Home from './pages/home';
-import InternalStyling from './pages/internal/styling';
-import Jobs from './pages/jobs';
 import Signin from './pages/signin';
-import NoSessionGuard from './router/NoSessionGuard';
+import Signup from './pages/signup';
+import { PrivateRoute, WithoutSessionRoute } from './router';
+
+const PrivateDashboard = (props: any) => {
+  return (
+    <PrivateRoute>
+      <Dashboard {...props} />
+    </PrivateRoute>
+  );
+};
 
 export default function App() {
   return (
     <Router root={Layout}>
-      <Route path="/" component={NoSessionGuard}>
+      <Route path="/dashboard" component={PrivateDashboard}>
+        <Route path="/inbox" component={() => <div>inbox</div>} />
+        <Route path="/jobs" component={() => <div>jobs</div>} />
+        <Route path="/applies" component={() => <div>applies</div>} />
+        <Route path="/statistics" component={() => <div>statistics</div>} />
+        <Route path="/settings" component={() => <div>settings</div>} />
+        <Route path="/" component={() => <div>dashboard</div>} />
+      </Route>
+
+      <Route path="/" component={WithoutSessionRoute}>
         <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/" component={Home} />
       </Route>
-      <Route path="/jobs/:id?" component={Jobs} />
-
-      <Route path="/internal">
-        <Route path="/styling" component={InternalStyling} />
-      </Route>
-
-      <Route path="/" component={Home} />
     </Router>
   );
 }
