5180a8e7b7d409a2919ccd35c8dbcc29062fcfb5
mrheio <drg.ov00@gmail.com>
2024-04-08 09:59:49 +0300

    improvement: Add context for drawer state

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,4 +1,4 @@
-import { Resource, createContext, createResource, useContext } from 'solid-js';
+import { Accessor, Resource, createContext, createResource, createSignal, useContext } from 'solid-js';
 import { getSession } from './api/authentication';
 import { User } from './types';
 
@@ -7,20 +7,48 @@ type SessionContextProps = {
   refetch: (info?: unknown) => User | Promise<User | null | undefined> | null | undefined;
 };
 
-const SessionContext = createContext<SessionContextProps>();
-
-export const Provider = (props: any) => {
-  const [session, { refetch }] = createResource(getSession);
-
-  return <SessionContext.Provider value={{ session, refetch }}>{props.children}</SessionContext.Provider>;
+type MenuDrawerContextProps = {
+  isOpen: Accessor<boolean>;
+  setOpen: (val: boolean) => void;
+  toggle: () => void;
 };
+
+const SessionContext = createContext<SessionContextProps>();
 
 export const useSessionContext = () => {
   const ctx = useContext(SessionContext);
 
   if (!ctx) {
+    console.error('useSessionContext must be used inside child of Provider');
     throw new Error('useSessionContext must be used inside child of Provider');
   }
 
   return ctx;
+};
+
+const MenuDrawerContext = createContext<MenuDrawerContextProps>();
+
+export const useMenu = () => {
+  const ctx = useContext(MenuDrawerContext);
+
+  if (!ctx) {
+    throw new Error('useMenu must be used inside child of Provider');
+  }
+
+  return ctx;
+};
+
+export const Provider = (props: any) => {
+  const [session, { refetch }] = createResource(getSession);
+  const [isMenuOpen, setIsMenuOpen] = createSignal(false);
+
+  return (
+    <SessionContext.Provider value={{ session, refetch }}>
+      <MenuDrawerContext.Provider
+        value={{ isOpen: isMenuOpen, setOpen: setIsMenuOpen, toggle: () => setIsMenuOpen((prev) => !prev) }}
+      >
+        {props.children}
+      </MenuDrawerContext.Provider>
+    </SessionContext.Provider>
+  );
 };
