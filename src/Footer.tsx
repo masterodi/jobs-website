81398d42407b3e094b367339eb15daf655fdc5af
mrheio <drg.ov00@gmail.com>
2024-04-08 09:56:00 +0300

    improvement: Update footer styles

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,17 +1,17 @@
 const Footer = () => {
   return (
-    <footer class="bg-primary-900">
-      <div class="flex-center container mx-auto min-h-[40vh] gap-4">
-        <a href="#" class="transition-md text-primary-50 hover:text-primary-200">
+    <footer class="bg-neutral-950">
+      <div class="flex-center container mx-auto min-h-[20vh] gap-4">
+        <a href="#" class="animated text-primary-50 hover:text-primary-200">
           About
         </a>
-        <a href="#" class="transition-md text-primary-50 hover:text-primary-200">
+        <a href="#" class="animated text-primary-50 hover:text-primary-200">
           Contact
         </a>
-        <a href="#" class="transition-md text-primary-50 hover:text-primary-200">
+        <a href="#" class="animated text-primary-50 hover:text-primary-200">
           Privacy Policy
         </a>
-        <a href="#" class="transition-md text-primary-50 hover:text-primary-200">
+        <a href="#" class="animated text-primary-50 hover:text-primary-200">
           What's next?
         </a>
       </div>
