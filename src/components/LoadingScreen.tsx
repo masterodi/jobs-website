ca15ba1c7fcecbc25a1541d33726d36ba6df8527
mrheio <drg.ov00@gmail.com>
2024-04-08 09:54:31 +0300

    refactor: Redesign LoadingScreen component

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import LoadingIndicator from './LoadingIndicator';
 
 const LoadingScreen: ParentComponent = (props) => {
   return (
-    <div class="grid min-h-screen place-items-center bg-gradient-to-b from-primary-500 to-primary-100">
+    <div class="grid min-h-screen place-items-center">
       <div>
         <div class="mx-auto h-12 w-12 [&>*]:h-full [&>*]:w-full">
           <LoadingIndicator />
