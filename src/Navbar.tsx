f87361b7ac34296eae5a80d9a82d3676f2bec736
mrheio <drg.ov00@gmail.com>
2024-04-08 09:57:50 +0300

    refactor: Redesign Navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,12 +1,16 @@
 import { useNavigate } from '@solidjs/router';
+import { BiRegularStats } from 'solid-icons/bi';
+import { FiBriefcase, FiHome, FiInbox, FiMenu, FiSettings, FiTriangle } from 'solid-icons/fi';
 import { Show } from 'solid-js';
-import { useSessionContext } from './Provider';
+import { useMenu, useSessionContext } from './Provider';
 import { signOutUser } from './api/authentication';
 import Button from './components/Button';
+import Drawer from './components/Drawer';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const { session, refetch } = useSessionContext();
+  const { isOpen: isMenuOpen, setOpen: setIsMenuOpen, toggle: toggleMenu } = useMenu();
 
   const signOut = async () => {
     await signOutUser();
@@ -14,32 +18,66 @@ const Navbar = () => {
   };
 
   return (
-    <nav class="sticky top-0 z-40 flex h-[60px] items-center justify-between gap-12 bg-white/75 px-4 backdrop-blur-sm">
-      <div class="flex items-center gap-8">
-        <h2 class="text-xl font-bold">
-          <a href="/">Place</a>
-        </h2>
-        <ul>
-          <li>
-            <a class="text-lg" href="/jobs">
-              Jobs
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <Show when={!session()}>
-          <Button slim variant="flat" onClick={() => navigate('/signin')}>
-            Get Started
-          </Button>
-        </Show>
-        <Show when={session()}>
-          <Button slim variant="flat" onClick={signOut}>
-            Sign out
-          </Button>
-        </Show>
-      </div>
-    </nav>
+    <>
+      <nav class="sticky top-0 z-30 flex h-[60px] items-center justify-between gap-12 bg-transparent px-4 backdrop-blur-md">
+        <div class="flex items-center gap-8">
+          <h2 class="title-4">
+            <a href="/">Jobbler</a>
+          </h2>
+        </div>
+        <div>
+          <Show when={session()}>
+            <Button variant="text" onClick={toggleMenu}>
+              <FiMenu />
+            </Button>
+          </Show>
+        </div>
+      </nav>
+
+      <Show when={session()}>
+        <Drawer side="right" open={isMenuOpen()} setOpen={setIsMenuOpen}>
+          <div class="p-2">
+            <ul>
+              <li>
+                <Button href="/dashboard" fluid variant="text" color="neutral" icon={<FiHome />}>
+                  Home
+                </Button>
+              </li>
+              <li>
+                <Button href="/dashboard/inbox" fluid variant="text" color="neutral" icon={<FiInbox />}>
+                  Inbox
+                </Button>
+              </li>
+              <li>
+                <Button href="/dashboard/jobs" fluid variant="text" color="neutral" icon={<FiBriefcase />}>
+                  Jobs
+                </Button>
+              </li>
+              <li>
+                <Button href="/dashboard/applies" fluid variant="text" color="neutral" icon={<FiTriangle />}>
+                  Applies
+                </Button>
+              </li>
+              <li>
+                <Button href="/dashboard/statistics" fluid variant="text" color="neutral" icon={<BiRegularStats />}>
+                  Statistics
+                </Button>
+              </li>
+              <li>
+                <Button href="/dashboard/settings" fluid variant="text" color="neutral" icon={<FiSettings />}>
+                  Settings
+                </Button>
+              </li>
+              <li>
+                <Button fluid variant="text" color="primary" onClick={signOut}>
+                  Sign Out
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </Drawer>
+      </Show>
+    </>
   );
 };
 
