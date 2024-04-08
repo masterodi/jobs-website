0e9e1338b356088da602819163ccaf7162247a27
mrheio <drg.ov00@gmail.com>
2024-04-08 09:50:44 +0300

    feat: Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,9 @@ export default {
           'url(https://images.unsplash.com/photo-1506784926709-22f1ec395907?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
       },
       colors: {
-        primary: colors.indigo,
-        neutral: colors.gray,
+        primary: colors.teal,
+        neutral: colors.zinc,
+        error: colors.rose,
       },
     },
   },
