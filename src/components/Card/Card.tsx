a9905ec154c24630356a7914c8462751e0c1e634
mrheio <drg.ov00@gmail.com>
2024-04-08 09:56:46 +0300

    refactor: Clean Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,29 +1,51 @@
-import { JSX, ParentComponent, ValidComponent, splitProps } from 'solid-js';
+import { A } from '@solidjs/router';
+import { cva } from 'class-variance-authority';
+import { ParentComponent, splitProps } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
 
-type CardProps = JSX.HTMLAttributes<HTMLDivElement> & {
-  onClick?: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent>;
-  active?: boolean;
-  variant: 'primary' | 'neutral';
-  as?: ValidComponent;
-  href?: string;
-};
+const cardVariants = cva(
+  ['flex', 'flex-col', 'rounded-lg', 'p-4', 'transition-all', 'duration-200', 'ease-in-out', 'hover:brightness-[101%]'],
+  {
+    variants: {
+      intent: {
+        primary: ['bg-primary-900', 'fill-white', 'text-white'],
+        neutral: ['bg-transparent-50', 'shadow-md', 'fill-black', 'text-black'],
+      },
+      clickable: {
+        false: '',
+        true: 'cursor-pointer',
+      },
+      active: {
+        false: 'border-2 border-transparent',
+        true: 'border-2 border-primary-500',
+      },
+    },
+    defaultVariants: {
+      intent: 'primary',
+      clickable: false,
+      active: false,
+    },
+  },
+);
 
-const Card: ParentComponent<CardProps> = (props) => {
-  const [local, other] = splitProps(props, ['as', 'onClick', 'variant', 'children', 'class', 'classList', 'active']);
+const Card: ParentComponent<any> = (props) => {
+  if ('href' in props) {
+    const [local, other] = splitProps(props, ['href', 'noScroll', 'variant', 'children']);
+
+    return (
+      <A href={local.href} class={cardVariants({ intent: local.variant })} noScroll={local.noScroll} {...other}>
+        {local.children}
+      </A>
+    );
+  }
+
+  const [local, other] = splitProps(props, ['as', 'onClick', 'variant', 'active', 'children']);
 
   return (
     <Dynamic
-      component={local.as ?? 'div'}
-      onClick={local.onClick}
-      class="flex flex-col rounded-lg p-4 transition-all duration-200 ease-in-out hover:brightness-[101%] "
-      classList={{
-        'cursor-pointer': !!local.onClick,
-        'border-2 border-transparent': local.active !== undefined && !local.active,
-        'border-2 border-primary-500': local.active,
-        'bg-primary-900 fill-white text-white': local.variant === 'primary',
-        'bg-transparent-50 shadow-md fill-black text-black': local.variant === 'neutral',
-      }}
+      component={local.as}
+      onClick={local.onClick as any}
+      class={cardVariants({ intent: local.active, active: local.active, clickable: !!local.onClick })}
       {...other}
     >
       {local.children}
